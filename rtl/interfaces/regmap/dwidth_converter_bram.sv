ccbe1706bc47f7f9dfd145d1ca36bde660b7d649
luisccc <luisccunha8@gmail.com>
2024-03-27 09:14:57 +0000

    refactor: Addition of byte enable control to the dwidth_converter

--- rtl/interfaces/regmap/dwidth_converter_bram.sv
+++ rtl/interfaces/regmap/dwidth_converter_bram.sv
@@ -33,6 +33,8 @@ module dwidth_converter_bram #(
     output logic [OUT_WIDTH - 1 : 0]   dout_o,
     input  logic [BRAM_DWIDTH - 1 : 0] dout_bram_i,
 
+    output logic [(BRAM_DWIDTH / 8) - 1 : 0] be_bram_o,
+
     // Info
     output logic                       valid_o,
     output logic                       ready_o
@@ -40,15 +42,16 @@ module dwidth_converter_bram #(
 
 // How many out_width fit into BRAM_width. 2^n divide by 2 to get the amount to shift
 localparam int unsigned ShiftAmount = (BRAM_DWIDTH/OUT_WIDTH) / 2;
+// How many bits are necessary for the bit width of the shift quantity
 localparam int unsigned BitWidthShift = $clog2(CONVERSION_RATIO);
 
 logic [BitWidthShift - 1 : 0] shift_qtty_n, shift_qtty_q;
-logic [7:0] actual_shift_qtty;  // For improved Verilatr compatibility
+logic [7:0] data_shift_qtty;  // For improved Verilatr compatibility
+logic [7:0] byte_en_shift_qtty;  // For improved Verilatr compatibility
 
 logic [ADDR_WIDTH-1:0] pipeline_addr_n, pipeline_addr_q;
 logic [OUT_WIDTH-1:0]  pipeline_data_n, pipeline_data_q;
 
-logic write_snd_stage_en_n, write_snd_stage_en_q;
 logic   read_snd_stage_en_n, read_snd_stage_en_q;
 
 always_comb begin
@@ -59,7 +62,6 @@ always_comb begin
     din_bram_o  = 0;
     dout_o      = 0;
 
-    write_snd_stage_en_n = 0;
     read_snd_stage_en_n  = 0;
 
     shift_qtty_n    = shift_qtty_q;
@@ -68,40 +70,34 @@ always_comb begin
 
     valid_o = 0;
     ready_o = 1;
-    actual_shift_qtty = 0;
+    data_shift_qtty = 0;
+    byte_en_shift_qtty = 0;
 
-    if(write_snd_stage_en_q) begin
-        // Replace the correct bits and write to the BRAM
+    if(read_snd_stage_en_q) begin
         // Verilatr does not like making the expression into one
-        actual_shift_qtty = (shift_qtty_q << $clog2(OUT_WIDTH));
+        data_shift_qtty = (shift_qtty_q << $clog2(OUT_WIDTH));
 
-        din_bram_o = dout_bram_i;
-        din_bram_o[actual_shift_qtty +: OUT_WIDTH] = pipeline_data_q;
-        // Correct address
-        addr_bram_o = pipeline_addr_q;
-
-        // Enable write
-        en_bram_o = 1;
-        we_bram_o = 1;
-        ready_o   = 0;
-    end else if(read_snd_stage_en_q) begin
-        // Verilatr does not like making the expression into one
-        actual_shift_qtty = (shift_qtty_q << $clog2(OUT_WIDTH));
-
-        dout_o  = dout_bram_i >> actual_shift_qtty;
+        dout_o  = dout_bram_i >> data_shift_qtty;
         valid_o = 1;
         ready_o = 0;
     end else if(en_i) begin
-        en_bram_o = 1; // Enable a read from the BRAM
+        en_bram_o = 1; // Enable BRAM
 
         // Get correct BRAM address
         addr_bram_o = addr_i >> ShiftAmount;
         shift_qtty_n  = addr_i[(BitWidthShift - 1) :0];
 
         if(we_i) begin
-            pipeline_addr_n = addr_bram_o;
-            pipeline_data_n = din_i;
-            write_snd_stage_en_n = 1;
+            // Replace the correct bits and write to the BRAM
+            // Verilatr does not like making the expression into one
+            data_shift_qtty = (shift_qtty_n << $clog2(OUT_WIDTH));
+            din_bram_o[data_shift_qtty +: OUT_WIDTH] = din_i;
+
+            // Put the byte enable bits into the right place
+            byte_en_shift_qtty = (shift_qtty_n << $clog2(OUT_WIDTH / 8));
+            be_bram_o = {(OUT_WIDTH / 8){1'b1}} << byte_en_shift_qtty;
+
+            we_bram_o = 1;
         end
         else
             read_snd_stage_en_n = 1;
@@ -113,13 +109,11 @@ always_ff @(posedge clk_i or negedge rst_ni) begin
     if (!rst_ni) begin
         pipeline_addr_q         <= 0;
         pipeline_data_q         <= 0;
-        write_snd_stage_en_q    <= 0;
         read_snd_stage_en_q     <= 0;
         shift_qtty_q            <= 0;
     end else begin
         pipeline_addr_q         <= pipeline_addr_n;
         pipeline_data_q         <= pipeline_data_n;
-        write_snd_stage_en_q    <= write_snd_stage_en_n;
         read_snd_stage_en_q     <= read_snd_stage_en_n;
         shift_qtty_q            <= shift_qtty_n;
     end
